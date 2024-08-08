acb7e4d850d56fd967cc9347eddd24b16e45d8ba
taiyo0622 <shawnleejunliang@gmail.com>
2024-08-08 15:24:55 +0800

    placed back firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,14 +7,14 @@ import { getStorage } from "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDqmhlCNAgn8bdOq4JY28fzG82hPMJ-BHc",
-  authDomain: "pcweb7-e795d.firebaseapp.com",
-  projectId: "pcweb7-e795d",
-  storageBucket: "pcweb7-e795d.appspot.com",
-  messagingSenderId: "179578883769",
-  appId: "1:179578883769:web:1e6dfdfef638b20bd65568"
-};
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
+  };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
